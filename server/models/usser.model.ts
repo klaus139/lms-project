fa7fb03b866d8ac4f2fbddee175b3557372e6724
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-12-14 10:44:09 +0100

    models done

--- server/models/usser.model.ts
+++ server/models/usser.model.ts
@@ -1,33 +1,81 @@
-import mongoose, {Document, Model, Schema} from 'mongoose';
-import bcrypt from 'bcryptjs';
+import mongoose, { Document, Model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+s/;
 
-export interface IUser extends Document{
-    name: string;
-    email: string;
-    password: string;
-    avatar: {
-        public_id: string;
-        url: string;
-    }
-    role: string;
-    isVerified: boolean;
-    courses: Array<{courseId: string}>;
-    comparePassword: (password: string) => Promise<boolean>;
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
 }
 
-const userSchema: Schema<IUser> = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, 'please enter your name']
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please enter your name"],
     },
-    email:{
-        type: String,
-        required: [true, 'please enter your email address'],
-        validate: {
-            validator: function(value:string){
-                return emailRegexPattern.test(value)
-        }
-    }
-})
+    email: {
+      type: String,
+      required: [true, "please enter your email address"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "please enter a valid email",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: [6, "password cannot be less than 6 characters"],
+      select: false, //hides the password field in the response when we get data from db
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+//hashpassword before saving
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//compare password
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+export default userModel;
